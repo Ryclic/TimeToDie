c7ff46336a17e74d8237b23e459f232ae545fb38
majik-coder <duongmtran11@gmail.com>
2022-02-11 21:20:01 -0800

    Add Crouch and Sprint
    added crouch and sprint mechanics, still need to mess around with values (especially for sprint jumping)

--- TimeToDie/Assets/Scripts/PlayerMovement.cs
+++ TimeToDie/Assets/Scripts/PlayerMovement.cs
@@ -14,12 +14,14 @@ public class PlayerMovement : MonoBehaviour
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
 
+    bool outSprint = false;
+    bool inSprint = false;
+
     Vector3 velocity;
     bool isGrounded;
 
     void Update()
     {
-        speed = 12f;
         // Reset velocity for on ground
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
         if(isGrounded && velocity.y < 0)
@@ -29,7 +31,38 @@ public class PlayerMovement : MonoBehaviour
         // Reduce velocity for mid-air movement
         if (!isGrounded)
         {
-            speed = 8f;
+            if (inSprint){
+                speed -=0.75f;
+            } else {
+                speed -=1.25f;
+            }
+            if (speed<=8f) speed = 8f;
+        //Crouching
+        } else if (Input.GetButton("Crouch"))
+        {
+            speed = 4f;
+
+
+
+        //Jumping
+        //Check if stop sprinting, if so slow down the player with min speed of 12
+        } else if (Input.GetButtonUp("Sprint") || outSprint){
+            inSprint = false;
+            outSprint = true;
+            speed -= 0.05f;
+            if (speed<=12f){
+                outSprint = false;
+                speed = 12f;
+            }
+        //If currently sprinting, increase the speed with max speed of 20
+        } else if (Input.GetButton("Sprint"))
+        {
+            inSprint = true;
+            speed += 1f;
+            if (speed>20f) speed = 20f;
+        } else
+        {
+            speed = 12f;
         }
         // WASD Movement
         float x = Input.GetAxis("Horizontal");
