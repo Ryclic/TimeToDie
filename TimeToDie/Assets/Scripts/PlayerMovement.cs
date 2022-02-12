8345ced09bd6ef8f540245aef97d5645a0e51331
Ryan Chen <86568468+RyanC175@users.noreply.github.com>
2022-02-11 20:12:58 -0800

    Add gravity
    Working version of simple gravity

--- TimeToDie/Assets/Scripts/PlayerMovement.cs
+++ TimeToDie/Assets/Scripts/PlayerMovement.cs
@@ -7,16 +7,33 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
 
     public float speed = 12f;
+    public float gravity = -9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
 
     void Update()
     {
+        // Reset velocity
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        // WASD Movement
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
-
-        // Multiply by time for framerate independent movement
+        
         controller.Move(move * speed * Time.deltaTime);
 
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
