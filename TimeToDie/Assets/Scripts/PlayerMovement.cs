0c494f71061bf1ab78e3e29cc5773b711465602d
Ryan Chen <86568468+RyanC175@users.noreply.github.com>
2022-02-11 20:31:37 -0800

    Add Jumping
    Working jump mechanic and reduced velocity while jumping/falling

--- TimeToDie/Assets/Scripts/PlayerMovement.cs
+++ TimeToDie/Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed = 12f;
     public float gravity = -9.81f;
+    public float jumpHeight = 3f;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
@@ -18,11 +19,17 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        // Reset velocity
+        speed = 12f;
+        // Reset velocity for on ground
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
         if(isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
+        }
+        // Reduce velocity for mid-air movement
+        if (!isGrounded)
+        {
+            speed = 8f;
         }
         // WASD Movement
         float x = Input.GetAxis("Horizontal");
@@ -31,6 +38,11 @@ public class PlayerMovement : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
         
         controller.Move(move * speed * Time.deltaTime);
+        // Jumping
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
 
         velocity.y += gravity * Time.deltaTime;
 
