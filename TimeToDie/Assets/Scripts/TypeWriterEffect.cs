7ad3b90b17fcc3e3a1c845f8e073c1f2c48524bd
Ryan Chen <86568468+RyanC175@users.noreply.github.com>
2022-02-19 20:26:32 -0800

    Add fence asset
    Free low-poly fence asset from unity store: https://assetstore.unity.com/packages/3d/props/exterior/low-poly-fence-pack-61661#description

--- TimeToDie/Assets/Scripts/TypeWriterEffect.cs
+++ TimeToDie/Assets/Scripts/TypeWriterEffect.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TypeWriter : MonoBehaviour
+public class TypeWriterEffect : MonoBehaviour
 {
     public float delay = 0.1f;
     public string fullText;
